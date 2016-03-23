52f79549c1192b1fbcee738563b82fa0c8313464
Budnikov Ivan <ufobudnikov@gmail.com>
2016-03-23 19:21:13 +0200

    Changed database access, removed target

--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/settings/Settings.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/settings/Settings.java
@@ -4,7 +4,7 @@ package nl.intratuin.settings;
  * Created by Иван on 19.03.2016.
  */
 public class Settings {
-    private static String host="192.168.1.23";
+    private static String host="192.168.10.210";
 
     public static String getHost(){ return host;}
 }
