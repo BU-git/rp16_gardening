6c7d6631a065518a1691ccdd27b200ce32814fbc
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2016-04-03 22:02:44 +0300

    [IN1-46] - Design for product search activity - most of, except some design issues

--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/SearchActivity.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/SearchActivity.java
@@ -2,25 +2,36 @@ package nl.intratuin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 
 import nl.intratuin.handlers.ProductAutoCompleteAdapter;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
     AutoCompleteTextView svIntratuin;
     ListView lSearch;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_search);
 
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.svIntratuin);
         textView.setThreshold(3);
         ProductAutoCompleteAdapter adapter = new ProductAutoCompleteAdapter(this);
         textView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
