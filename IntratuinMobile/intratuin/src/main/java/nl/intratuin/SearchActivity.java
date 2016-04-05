a7d7e1dfd778e3af37e5bdadea2da116b55d7a96
Aleksander Chugui <aleksandrchugui@gmail.com>
2016-04-05 16:27:21 +0300

    [IN1-52, IN1-53] - activity for scanner created, scandit sdk added

--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/SearchActivity.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/SearchActivity.java
@@ -1,21 +1,5 @@
 package nl.intratuin;
 
-<<<<<<< HEAD
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.ListView;
-import android.widget.SearchView;
-
-public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener,
-        SearchView.OnCloseListener {
-
-    SearchView svIntratuin;
-    ListView lSearch;
-    // Progress Dialog
-    private ProgressDialog pDialog;
-=======
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -38,63 +22,10 @@ import nl.intratuin.net.UriConstructor;
 public class SearchActivity extends AppCompatActivity{
     ListView lSearch;
     String[] categories;
->>>>>>> 2bae88b5df9a58aefce22a28e19a5c75f5dffdfa
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.activity_search);
-
-        svIntratuin = (SearchView) findViewById(R.id.svIntratuin);
-        lSearch = (ListView) findViewById(R.id.lSearch);
-
-        svIntratuin.setOnQueryTextListener(this);
-        svIntratuin.setOnCloseListener(this);
-
-        new JsonSearchTask().execute();
-
-    }
-
-    public boolean onQueryTextChange(String newText) {
-
-        return false;
-    }
-
-    public boolean onQueryTextSubmit(String query) {
-
-        return false;
-    }
-
-    public boolean onClose() {
-
-        return false;
-    }
-
-    private class JsonSearchTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(SearchActivity.this);
-            pDialog.setMessage("@string/loading");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(false);
-            pDialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-        }
-    }
-}
-=======
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_search);
@@ -130,4 +61,3 @@ public class SearchActivity extends AppCompatActivity{
         });
     }
 }
->>>>>>> 2bae88b5df9a58aefce22a28e19a5c75f5dffdfa
