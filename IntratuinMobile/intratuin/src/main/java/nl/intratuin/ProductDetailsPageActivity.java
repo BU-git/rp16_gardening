e86b4614a35d8ba071e52d45b5ba271245048912
Budnikov Ivan <ufobudnikov@gmail.com>
2016-04-06 20:31:43 +0300

    [IN1-49] - Product page design -- need to be checked on tablets

--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/ProductDetailsPageActivity.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/ProductDetailsPageActivity.java
@@ -2,7 +2,9 @@ package nl.intratuin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -18,6 +20,11 @@ public class ProductDetailsPageActivity extends AppCompatActivity {
         TextView tvProductName = (TextView) findViewById(R.id.tvProductName);
         ImageView ivProductImage = (ImageView) findViewById(R.id.ivProductImage);
         TextView tvProductPrice = (TextView) findViewById(R.id.tvProductPrice);
+        ImageView ivRelated1 = (ImageView) findViewById(R.id.ivRelated1);
+        ImageView ivRelated2 = (ImageView) findViewById(R.id.ivRelated2);
+        ImageView ivRelated3 = (ImageView) findViewById(R.id.ivRelated3);
+        ImageView ivRelated4 = (ImageView) findViewById(R.id.ivRelated4);
+        Spinner sSize = (Spinner) findViewById(R.id.sSize);
 
         tvProductName.setText(getIntent().getExtras().getString("productName"));
         tvProductPrice.setText("" + getIntent().getExtras().getDouble("productPrice"));
@@ -26,5 +33,32 @@ public class ProductDetailsPageActivity extends AppCompatActivity {
                 .resize(500, 500)
                 .centerCrop()
                 .into(ivProductImage);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.size, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sSize.setAdapter(adapter);
+
+        Picasso.with(this)
+                .load(getIntent().getExtras().getString("productImage"))
+                .resize(80, 80)
+                .centerCrop()
+                .into(ivRelated1);
+        Picasso.with(this)
+                .load(getIntent().getExtras().getString("productImage"))
+                .resize(80, 80)
+                .centerCrop()
+                .into(ivRelated2);
+        Picasso.with(this)
+                .load(getIntent().getExtras().getString("productImage"))
+                .resize(80, 80)
+                .centerCrop()
+                .into(ivRelated3);
+        Picasso.with(this)
+                .load(getIntent().getExtras().getString("productImage"))
+                .resize(80, 80)
+                .centerCrop()
+                .into(ivRelated4);
     }
 }
