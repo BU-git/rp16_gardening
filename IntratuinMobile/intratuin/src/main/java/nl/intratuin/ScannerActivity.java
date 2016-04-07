5910f3ba06451f474df2a6e3cfc04d04e726bf6a
Aleksander Chugui <aleksandrchugui@gmail.com>
2016-04-07 13:15:43 +0300

    [IN-52,IN1-53] - barcode scanning. needs to test launching

--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/ScannerActivity.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/ScannerActivity.java
@@ -4,55 +4,50 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.scandit.barcodepicker.BarcodePicker;
-import com.scandit.barcodepicker.OnScanListener;
-import com.scandit.barcodepicker.ScanSession;
-import com.scandit.barcodepicker.ScanSettings;
-import com.scandit.barcodepicker.ScanditLicense;
-import com.scandit.recognition.Barcode;
+import com.mirasense.scanditsdk.ScanditSDKAutoAdjustingBarcodePicker;
+import com.mirasense.scanditsdk.ScanditSDKBarcodePicker;
+import com.mirasense.scanditsdk.interfaces.ScanditSDKListener;
 
-public class ScannerActivity extends AppCompatActivity implements OnScanListener {
+public class ScannerActivity extends AppCompatActivity implements ScanditSDKListener {
 
-    private BarcodePicker mPicker;
+    private ScanditSDKAutoAdjustingBarcodePicker picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_scanner);
 
-        // Set your app key
-        ScanditLicense.setAppKey("0Oq9DpP9jIjYPPYEQCHc+5juV/BzKowcmd2bTWoQYaI");
-        ScanSettings settings = ScanSettings.create();
-        settings.setSymbologyEnabled(Barcode.SYMBOLOGY_EAN13, true);
-        settings.setSymbologyEnabled(Barcode.SYMBOLOGY_UPCA, true);
-
-        // Instantiate the barcode picker by using the settings defined above.
-        BarcodePicker picker = new BarcodePicker(this, settings);
-
-        // Set the on scan listener to receive barcode scan events.
-        picker.setOnScanListener(this);
+        // Setup the barcode scanner
+        picker = new ScanditSDKAutoAdjustingBarcodePicker(this, "0Oq9DpP9jIjYPPYEQCHc+5juV/BzKowcmd2bTWoQYaI", ScanditSDKBarcodePicker.CAMERA_FACING_BACK);
+        setContentView(picker);
+        picker.getOverlayView().addListener(this);
+        picker.startScanning();
     }
 
-    public void onClick(View view) {
-        switch (view.getId()) {
-
-        }
-    }
 
     @Override
     protected void onResume() {
-        mPicker.startScanning();
+        picker.startScanning();
         super.onResume();
     }
     @Override
     protected void onPause() {
-        mPicker.stopScanning();
+        picker.stopScanning();
         super.onPause();
     }
 
     @Override
-    public void didScan(ScanSession scanSession) {
+    public void didCancel() {
+
+    }
+
+    @Override
+    public void didScanBarcode(String s, String s1) {
+
+    }
+
+    @Override
+    public void didManualSearch(String s) {
 
     }
 }
--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/SearchActivity.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/SearchActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,9 +21,12 @@ import nl.intratuin.dto.Product;
 import nl.intratuin.handlers.ProductAutoCompleteAdapter;
 import nl.intratuin.net.UriConstructor;
 
-public class SearchActivity extends AppCompatActivity{
+public class SearchActivity extends AppCompatActivity implements OnClickListener {
     ListView lSearch;
     String[] categories;
+    ImageButton bSearchBarcode;
+    ImageButton bSearchMan;
+    ImageButton bSearchBasket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +35,16 @@ public class SearchActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_search);
 
+        bSearchBarcode = (ImageButton)findViewById(R.id.bSearchBarcode);
+        bSearchMan = (ImageButton)findViewById(R.id.bSearchMan);
+        bSearchBasket = (ImageButton)findViewById(R.id.bSearchBasket);
         lSearch = (ListView) findViewById(R.id.lSearch);
         categories = getResources().getStringArray(R.array.categories);
         final ArrayAdapter<String> adapterCategories = new ArrayAdapter<String>(this, R.layout.activity_categories, android.R.id.text1, categories);
+
+        bSearchBasket.setOnClickListener(this);
+        bSearchMan.setOnClickListener(this);
+        bSearchBasket.setOnClickListener(this);
         lSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
@@ -59,5 +71,23 @@ public class SearchActivity extends AppCompatActivity{
                 startActivity(productPadeIntent);
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bSearchBarcode:
+                Intent scannerIntent = new Intent(this, ScannerActivity.class);
+                startActivity(scannerIntent);
+                break;
+
+            case R.id.bSearchMan:
+
+                break;
+
+            case R.id.bSearchBasket:
+
+                break;
+        }
     }
 }
