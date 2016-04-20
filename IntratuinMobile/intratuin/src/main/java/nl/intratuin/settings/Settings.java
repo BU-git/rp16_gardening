3cc4c824da877db013a8199320832d0946abdd5a
Budnikov Ivan <ufobudnikov@gmail.com>
2016-04-20 18:11:02 +0300

    [undoc] - Removed password encryption because it not used in API

--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/settings/Settings.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/settings/Settings.java
@@ -1,13 +1,6 @@
 package nl.intratuin.settings;
 
-import android.util.Base64;
-
 import com.twitter.sdk.android.core.TwitterAuthConfig;
-
-import java.security.SignatureException;
-
-import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.Mac;
 
 /**
  * Created by Иван on 19.03.2016.
@@ -17,7 +10,7 @@ public class Settings {
     private static final String twitter_par1 = "CpdWZOr27su9Vduhipt3XVUgD";
     private static final String twitter_par2 = "pcOrskD4hWcUGxi5n3nMvtL3bArhIuyj2pmiMCQ7E9jj71wH5w";
 
-    private static boolean useDeployed = false;
+    private static boolean useDeployed = true;
 
     private static int connectionTimeout=10000;
 
@@ -28,26 +21,6 @@ public class Settings {
     public static String getHost(){ return useDeployed?hostBionic:hostLocal;}
     public static TwitterAuthConfig getTwitterConfig() {
         return new TwitterAuthConfig(twitter_par1,twitter_par2);
-    }
-    public static String sha1(String data, String key) throws SignatureException{
-        String result="";
-        try {
-            // get an hmac_sha1 key from the raw key bytes
-            SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), "HmacSHA1");
-
-            // get an hmac_sha1 Mac instance and initialize with the signing key
-            Mac mac = Mac.getInstance("HmacSHA1");
-            mac.init(signingKey);
-
-            // compute the hmac on input data bytes
-            byte[] rawHmac = mac.doFinal(data.getBytes());
-
-            // base64-encode the hmac
-            result = Base64.encodeToString(rawHmac,Base64.DEFAULT);
-        } catch (Exception e) {
-            throw new SignatureException("Failed to generate HMAC : " + e.getMessage());
-        }
-        return result.substring(0,result.length()-1);
     }
 
     public static int getConnectionTimeout() { return connectionTimeout; }
