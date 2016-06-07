5bb97f7dc9d331a11de7d477fc97a3e46aa4bb7a
MariiaMaltseva <M.Maltseva23@gmail.com>
2016-06-07 23:05:16 +0300

    cache product

--- IntratuinMobile/intratuin/src/main/java/nl/intratuin/ProductDetailsPageActivity.java
+++ IntratuinMobile/intratuin/src/main/java/nl/intratuin/ProductDetailsPageActivity.java
@@ -33,11 +33,7 @@ public class ProductDetailsPageActivity extends ToolBarActivity {
         TextView tvProductName = (TextView) findViewById(R.id.tvProductName);
         ImageView ivProductImage = (ImageView) findViewById(R.id.ivProductImage);
         TextView tvProductPrice = (TextView) findViewById(R.id.tvProductPrice);
-        ImageView ivRelated1 = (ImageView) findViewById(R.id.ivRelated1);
-        ImageView ivRelated2 = (ImageView) findViewById(R.id.ivRelated2);
-        ImageView ivRelated3 = (ImageView) findViewById(R.id.ivRelated3);
-        ImageView ivRelated4 = (ImageView) findViewById(R.id.ivRelated4);
-        Spinner sSize = (Spinner) findViewById(R.id.sSize);
+        TextView tvPrice = (TextView) findViewById(R.id.tvPrice);
 
         final Bundle extra = getIntent().getExtras();
         if (extra != null) {
@@ -48,37 +44,9 @@ public class ProductDetailsPageActivity extends ToolBarActivity {
         tvProductPrice.setText("" + productBySearch.getProductPrice());
         Picasso.with(this)
                 .load(productBySearch.getProductImage())
-                .resize(500, 500)
+                .resize(300, 300)
                 .centerCrop()
                 .into(ivProductImage);
 
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.size, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sSize.setAdapter(adapter);
-
-        //aaaaaa, what's that?
-        //it's magic, don't touch
-        Picasso.with(this)
-                .load(getIntent().getExtras().getString("productImage"))
-                .resize(80, 80)
-                .centerCrop()
-                .into(ivRelated1);
-        Picasso.with(this)
-                .load(getIntent().getExtras().getString("productImage"))
-                .resize(80, 80)
-                .centerCrop()
-                .into(ivRelated2);
-        Picasso.with(this)
-                .load(getIntent().getExtras().getString("productImage"))
-                .resize(80, 80)
-                .centerCrop()
-                .into(ivRelated3);
-        Picasso.with(this)
-                .load(getIntent().getExtras().getString("productImage"))
-                .resize(80, 80)
-                .centerCrop()
-                .into(ivRelated4);
     }
 }
